1bce8714d681121d84a75a840521010fa6fc83fa
Raphael Tang <raphpb1912@gmail.com>
2024-09-17 23:04:14 +0800

    feat: add math.js

--- bot/functions/main.ts
+++ bot/functions/main.ts
@@ -5,6 +5,7 @@ import { eq } from "drizzle-orm";
 import fs from "fs";
 import got from "got";
 import { InputFile } from "grammy";
+import * as mathjs from "mathjs";
 import path from "path";
 import Replicate from "replicate";
 import { pipeline as streamPipeline } from "stream/promises";
@@ -463,6 +464,16 @@ export const mainFunctions = (
 
         return "Updated personality, notified user. Continue function calls.";
       },
+    }),
+
+    calculate: tool({
+      description: [
+        "Evaluate mathematical expressions.",
+        "Example expressions:",
+        "'1.2 * (2 + 4.5)', '12.7 cm to inch', 'sin(45 deg) ^ 2'.",
+      ].join(" "),
+      parameters: z.object({ expression: z.string() }),
+      execute: async ({ expression }) => mathjs.evaluate(expression),
     }),
   };
 };
