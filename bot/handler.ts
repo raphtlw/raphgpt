69648c3b0433bfa0c36c69ef52109fd602979356
Raphael Tang <raphpb1912@gmail.com>
2025-01-08 04:20:22 +0800

    feat: modified pricing model

--- bot/handler.ts
+++ bot/handler.ts
@@ -908,8 +908,8 @@ ${italic(`You can get more tokens from the store (/topup)`)}`,
     cost += usage.promptTokens * (2.5 / 1_000_000);
     cost += usage.completionTokens * (10 / 1_000_000);
 
-    // up to 50 cents in fees per message (the more tokens used, the less fees)
-    cost += cost + 0.3 / usage.completionTokens + 0.2;
+    // 50% will be taken as fees
+    cost += (cost / 100) * 50;
 
     cost /= Math.pow(10, 2); // Store value without 2 d.p.
 
